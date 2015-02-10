f325d7189f6140f20d8cffd7aa891aad53197f67
HampusEngstrom <ael10hen@student.lu.se>
2015-02-10 17:26:27 +0100

    EstimoteSetup

--- app/src/main/java/com/positioning/indoor/indoorpositioning/OfflineActivity.java
+++ app/src/main/java/com/positioning/indoor/indoorpositioning/OfflineActivity.java
@@ -1,14 +1,27 @@
 package com.positioning.indoor.indoorpositioning;
 
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
 
 /**
  * Created by Hampus on 2015-02-10.
  */
 public class OfflineActivity extends ActionBarActivity{
+    private BeaconManager beaconManager;
+    private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+    private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId", ESTIMOTE_PROXIMITY_UUID, null, null);
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,38 +44,94 @@ public class OfflineActivity extends ActionBarActivity{
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void setupEstimote() {
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        if (!beaconManager.isBluetoothEnabled())
+            Toast.makeText(getApplicationContext(), "Enable Bluetooth", Toast.LENGTH_SHORT).show();
+
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
+                for (Beacon b : beacons) {
+
+                }
+           }
+            });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupEstimote();
     }
 
     @Override
     protected void onStop() {
+        try {
+            beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
+        } catch (RemoteException e) {
+            Log.e("Beacon", "Cannot stop but it does not matter now", e);
+        }
         super.onStop();
     }
 
     @Override
     protected void onStart() {
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
+                } catch (RemoteException e) {
+                    Log.e("Beacon", "Cannot start ranging", e);
+                }
+            }
+        });
         super.onStart();
     }
 
     @Override
     protected void onPause() {
+        try {
+            beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
+        } catch (RemoteException e) {
+            Log.e("Beacon", "Cannot stop but it does not matter now", e);
+        }
         super.onPause();
     }
 
     @Override
     protected void onResume() {
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
+                } catch (RemoteException e) {
+                    Log.e("Beacon", "Cannot start ranging", e);
+                }
+            }
+        });
         super.onResume();
     }
 
     @Override
     protected void onRestart() {
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
+                } catch (RemoteException e) {
+                    Log.e("Beacon", "Cannot start ranging", e);
+                }
+            }
+        });
         super.onRestart();
     }
 
     @Override
     protected void onDestroy() {
+        beaconManager.disconnect();
         super.onDestroy();
     }
 }
