34fa104fe2b4c183289a91f0778f1bacf13829dc
FredrikHelander <gda10fhe@student.lu.se>
2015-02-10 21:29:10 +0100

    NN

--- app/src/main/java/com/positioning/indoor/indoorpositioning/Filter.java
+++ app/src/main/java/com/positioning/indoor/indoorpositioning/Filter.java
@@ -4,6 +4,10 @@ package com.positioning.indoor.indoorpositioning;
  * Created by fredrik on 2015-02-10.
  */
 public class Filter {
+    private double[] sample;
+    private double[] state;
+    public Filter(double[] sample) {
+        this.sample = sample;
+    }
 
-    
 }
---
+++ app/src/main/java/com/positioning/indoor/indoorpositioning/Nearestneighbor.java
@@ -0,0 +1,39 @@
+package com.positioning.indoor.indoorpositioning;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by fredrik on 2015-02-10.
+ */
+public class Nearestneighbor {
+    public ArrayList<double[]> rp;
+
+    public Nearestneighbor(ArrayList<double[]> rp) {
+        this.rp = rp;
+    }
+
+    public int getLocation(double[] v) {
+        double[] dist = new double[rp.size()];
+        for(int i = 0; i < rp.size(); i++) {
+            dist[i] = Euclidean(v, rp.get(i));
+        }
+        double[] temp = dist;
+        Arrays.sort(dist);
+        for(int j = 0; j < rp.size(); j++) {  //Hmm
+            if(temp[j] == dist[0]) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    public double Euclidean(double[] s, double[] v) {
+        double distance = 0;
+        for(int i = 0; i < 6; i++) {     //6 was size of sample
+            distance += Math.pow((s[i]-v[i]), 2);
+        }
+        distance = Math.sqrt(distance);
+        return distance;
+    }
+}
